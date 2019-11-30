dd9763393bea998e0c91997e264bc4a4b250392b
BakayDev <gamempvs@gmail.com>
2019-11-30 21:36:24 +0600

    added chats migrations

---
+++ database/migrations/2019_11_30_153336_create_chats_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chats', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('message_id');
+            $table->unsignedBigInteger('session_id');
+            $table->unsignedBigInteger('user_id');
+            $table->boolean('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chats');
+    }
+}
