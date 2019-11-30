a0d63af3b01af601a1716f66f4523b8a182d6194
BakayDev <gamempvs@gmail.com>
2019-12-01 01:35:29 +0600

    + feature chat scroll

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,7 +7,7 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
-
+Vue.use(require('vue-chat-scroll'));
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
--- resources/js/components/MessageComponent.vue
+++ resources/js/components/MessageComponent.vue
@@ -3,10 +3,12 @@
         <div class="card-header">
             Chats
         </div>
-        <div class="card-body">
-            hello Test
+        <div class="card-body" v-chat-scroll>
+           <p class="card-text" v-for="chat in chats" :key="chat.message">
+               {{chat.message}}
+           </p>
         </div>
-        <form class="card-footer">
+        <form class="card-footer" @submit="send">
             <div class="form-group">
                 <input type="text" class="form-control" placeholder="Write your message here">
             </div>
@@ -19,12 +21,35 @@
 
 <script>
     export default {
-
+        data() {
+            return {
+                chats: []
+            }
+        },
+        methods: {
+            send() {
+                console.log('awd')
+            }
+        },
+        created() {
+            this.chats.push({message: 'Hello'},
+                {message: 'How are you1?'},
+                {message: 'How are you2?'},
+                {message: 'How are you3?'},
+                {message: 'How are you4?'},
+                {message: 'How are you5?'},
+                {message: 'How are you6?'},
+           )
+        }
     }
 </script>
 
 <style>
     .chat-box {
         height: 400px;
+    }
+
+    .card-body {
+        overflow-y: scroll;
     }
 </style>
