c128d2ca50a63b7b2770f1b029b212306d51a381
BakayDev <gamempvs@gmail.com>
2019-12-02 01:45:38 +0600

    installed laravel-echo pusher js

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,6 +38,7 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'useTLS' => true,
+                'encrypted' => true,
             ],
         ],
 
