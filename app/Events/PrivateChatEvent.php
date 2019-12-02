c002f5a7670ef4e0c2d136e80e57fd15456265db
BakayDev <gamempvs@gmail.com>
2019-12-02 22:16:59 +0600

    Create Event listener

---
+++ app/Events/PrivateChatEvent.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class PrivateChatEvent implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $content;
+    public $chat;
+
+
+    public function __construct($content, $chat)
+    {
+        $this->content = $content;
+        $this->chat = $chat;
+        $this->dontBroadcastToCurrentUser();
+    }
+
+
+    public function broadcastOn()
+    {
+        return new PrivateChannel('Chat.'. $this->chat->session_id);
+    }
+}
