254b8e0c09345973a3a94e5a4e243aceb3fbaed0
BakayDev <gamempvs@gmail.com>
2019-12-02 16:05:50 +0600

    Create Laravel session event

---
+++ app/Events/SessionEvent.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class SessionEvent implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $session;
+    public $session_by;
+
+
+    public function __construct($session, $session_by)
+    {
+        $this->session = $session;
+        $this->session_by = $session_by;
+    }
+
+
+    public function broadcastOn()
+    {
+        return new Channel('Chat');
+    }
+}
